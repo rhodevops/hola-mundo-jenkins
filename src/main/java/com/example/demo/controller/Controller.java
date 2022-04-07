9cef365ff0d2241acd5a169115734a51146ccbb0
Roberto Negro <rhonegro@gmail.com>
2022-04-07 13:01:12 +0200

    añadir hola blueocean

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
 	@GetMapping
 	public String saludo() {
 		
-		return "Hola mundo, hola jenkins, hola docker";
+		return "Hola mundo, hola jenkins, hola blueocean";
 	}
 
 }
